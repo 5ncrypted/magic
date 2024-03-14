41429b3f59dbb2063c24c03eb4856263edfb5b6a
AlphaDev <91440168+5ncrypted@users.noreply.github.com>
2024-03-14 15:10:00 -0500

    added clarifying punctuation for funzies

--- scriptlets.js
+++ scriptlets.js
@@ -5,4 +5,4 @@ alertB.js text/javascript
 (function(){alert('test2');})();
 
 dataTestA.js text/javascript
-(function(){window.onload = function(){document.open('text/html');document.write('<div style="width: 100%; text-align: center; font-size: 30px">yea I made a thing and guess what it can modify web pages</div>');document.close();}})();
+(function(){window.onload = function(){document.open('text/html');document.write('<div style="width: 100%; text-align: center; font-size: 30px">yea I made a thing and guess what? it can modify web pages</div>');document.close();}})();
