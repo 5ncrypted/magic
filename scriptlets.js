0108b0d862b8244c77b28b36bf13cea257048561
AlphaDev <91440168+5ncrypted@users.noreply.github.com>
2024-03-14 15:05:17 -0500

    added another cool script to my test batch

--- scriptlets.js
+++ scriptlets.js
@@ -3,3 +3,6 @@ alertA.js text/javascript
 
 alertB.js text/javascript
 (function(){alert('test2');})();
+
+dataTestA.js text/javascript
+(function(){document.body.innerHTML = '<div style="width: 100%; text-align: center; font-size: 30px">yea I made a thing and guess what it can modify web pages</div>'})();
